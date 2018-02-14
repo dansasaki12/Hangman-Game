a2676306be3a8e69d6cb3b4a2aed83b93255feb4
Dan Sasaki <dansasaki@dans-mbp.uconnect.utah.edu>
2018-02-13 19:23:11 -0700

    update

---
+++ assets/javascript/game.js
@@ -0,0 +1,44 @@
+
+(function() {
+
+    var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+    'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+    var word ;  //selected word
+    var guesses ; //stored guesses
+    var spaces ; //number of spaces in word
+    var counter ; //correct guesses
+    var lives ; //number of lives
+
+    //words for the game
+    var hangmanwords = ["wrench", "socket", "hammer", "screwdriver"]
+
+    //random selection of words
+    var words = hangmanwords[Math.floor(Math.random)() * hangmanwords.length)];
+    words = chosenCategory[Math.floor(Math.random() * chosenCategory.length)];
+    words = words.replace(/\s/g, "-");
+
+// var showLives = document.getElementById("mylives");
+
+
+//  var buttons = function () {
+//       myButtons = document.getElementById('buttons');
+//       letters = document.createElement('ul');
+  
+//       for (var i = 0; i < alphabet.length; i++) {
+//         letters.id = 'alphabet';
+//         list = document.createElement('li');
+//         list.id = 'letter';
+//         list.innerHTML = alphabet[i];
+//         check();
+//         myButtons.appendChild(letters);
+//         letters.appendChild(list);
+//       }
+//     }
+
+for (var i = 0; i < \variable\.length; i++){
+    
+}
+})();
+
