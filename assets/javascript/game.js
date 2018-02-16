051edbfc90162947559031be2aec150ad842798e
Dan Sasaki <dansasaki@Dans-MacBook-Pro.local>
2018-02-16 16:57:28 -0700

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -38,9 +38,9 @@ document.onkeypress = function(event) {
 		randomLetter = alphabetLetters[Math.floor(Math.random() * alphabetLetters.length)];
 		console.log("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + guessesLeft + " Guesses so far: " + guessesSoFar + " Computer picked: " + randomLetter);
 	}
-    var html = "<br><br><br><br><p><h1>Psychic Game</h1></p>" + "<p><h4>What letter I\'m thinking of</h4></p>" + 
-    "<p><h4>Wins: " + wins + "</h4></p>" + "<p><h4>Losses: " + losses + "</h4></p>" + 
-    "<p><h4>Guesses Left: " + guessesLeft + "</h4></p>" + 
-    "<p><h4>Your guesses so far: " + guessesSoFar + "</h4></p>";
+    var html = "<br><br><br><br><br><br><br><p><h1>Psychic Game</h1></p>" + "<p><h5>What letter I\'m thinking of</h5></p>" + 
+    "<p><h5>Wins: " + wins + "</h5></p>" + "<p><h5>Losses: " + losses + "</h5></p>" + 
+    "<p><h5>Guesses Left: " + guessesLeft + "</h5></p>" + 
+    "<p><h5>Your guesses so far: " + guessesSoFar + "</h5></p>";
 	document.querySelector("#game").innerHTML = html;
 }
